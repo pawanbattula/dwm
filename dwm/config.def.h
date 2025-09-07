8023e218fc4a7f420c1a2ef20dd0cd1bf6138edb
PawanBattula <pawank.battula@gmail.com>
2025-09-07 17:13:06 +0530

    Minor Tweaks

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int refresh_rate        = 60;  /* matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions */
+static const unsigned int refresh_rate	= 60;  /* matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 26;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -14,7 +14,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 #define ICONSIZE                     24     /* icon size */
 #define ICONSPACING                   2      /* space between icon and title */
 #define SHOWWINICON                   1      /* 0 means no winicon */
-static const char *fonts[]          = { "MesloLGS Nerd Font Mono:size=14", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "MesloLGS Nerd Font Mono:size=14:antialias=true:autohint=true:hintstyle=hintfull", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
 /* static const char dmenufont[]       = "monospace:size=12"; */
 static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#3B4252";
@@ -28,13 +28,15 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
+	"dbus-update-activation-environment", "--systemd", "--all", NULL,
 	"numlockx", "s", "on", NULL,
 	"xset", "s", "off", NULL,
 	"xset", "s", "noblank", NULL,
         "xset", "-dpms", NULL,
 	"lxpolkit", NULL,
 	"flameshot", NULL,
-	"sh", "-c", "picom --experimental-backends --config ~/.config/picom/picom.conf", NULL,
+	"dunst", NULL,
+	"sh", "-c", "picom  --config ~/.config/picom/picom.conf", NULL,
 	"sh", "-c", "feh --randomize --bg-fill /$HOME/.config/walls/*", NULL,
 	"pasystray", NULL,
 	"nm-applet", NULL,
@@ -56,7 +58,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -83,7 +85,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "ghostty", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
@@ -91,7 +93,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_x, 	   spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_b,	   spawn,	   SHCMD ("brave-browser") },
-	{ MODKEY|ShiftMask,		XK_b,	   spawn,	   SHCMD ("thorium-browser") },
+	{ MODKEY|ShiftMask,		XK_b,	   spawn,	   SHCMD ("google-chrome-stable") },
 	{ MODKEY|ControlMask|ShiftMask, XK_b,      togglebar,      {0} },
 	{ MODKEY,			XK_e,	   spawn,	   SHCMD ("thunar") },
 	{ MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD ("feh --randomize --bg-fill /$HOME/.config/walls/*")},
